3ee02ca6988b46b717f03e338e262eabde54027c
user <madhureddy97@gmail.com>
2020-05-12 09:37:53 +0530

    my code

---
+++ SamplJunit2/src/com/sample/junit/JunitTests.java
@@ -0,0 +1,21 @@
+package com.sample.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.sample.calculation.Calculation;
+
+public class JunitTests {
+	@Test  
+    public void testFindMax1(){  
+        assertEquals(4,Calculation.findMax(new int[]{1,3,4,2}));  
+        assertEquals(-1,Calculation.findMax(new int[]{-12,-1,-3,-4,-2}));  
+    }  
+	
+	@Test  
+    public void testFindMax2(){  
+        assertEquals(4,Calculation.findMax(new int[]{1,3,4,2}));  
+        assertEquals(0,Calculation.findMax(new int[]{-12,-1,-3,-4,-2}));  
+    }  
+}
